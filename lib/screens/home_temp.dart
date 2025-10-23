edad811132fb4185c3d797fe4ea22c539620a7e2
WGitIJ <wisdomimade@paucasesnovescifp.cat>
2025-10-23 09:52:33 +0200

    Refactor HomePageTemp to use elementsMap for dynamic ListTile creation

--- lib/screens/home_temp.dart
+++ lib/screens/home_temp.dart
@@ -1,28 +1,45 @@
 import 'package:flutter/material.dart';
 
 class HomePageTemp extends StatelessWidget {
-  const HomePageTemp({super.key});
   final elements = ['Title 1', 'Title 2', 'Title 3'];
+  final elementsMap = ['Contact 1', 'Contact 2', 'Contact 3'];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Components Temp'),
-      ),
-      body: ListView(
-        children: [
-          ListTile(
-            title: Text('Title 1'),
-          ),
-          ListTile(
-            title: Text('Title 2'),
-          ),
-          ListTile(
-            title: Text('Title 3'),
-          ),
-        ],
-      ),
+      appBar: AppBar(title: Text('Components Temp')),
+      body: ListView(children: _crearElementosMap()),
     );
   }
-}  
+
+  // Métodos
+  List<Widget> _crearElementos() {
+    List<Widget> lista = [];
+    for (String element in elements) {
+      final tempWidget = ListTile(title: Text(element));
+      lista.add(tempWidget);
+      lista.add(Divider());
+    }
+
+    return lista;
+  }
+
+  // Alternativa usando Map
+  List<Widget> _crearElementosMap() {
+    var widgets = elementsMap.map((element) {
+      return Column(
+        children: [
+          ListTile(
+            leading: Icon(Icons.person),
+            title: Text(element),
+            trailing: Icon(Icons.more_vert),
+            onTap: () => print('Tapped on $element'),
+          ),
+          
+        ],
+      );
+    });
+
+    return widgets.toList();
+  }
+}
