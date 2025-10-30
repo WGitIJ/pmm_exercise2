b3b4b6aac7adc50575704323b5d653acc516125b
WGitIJ <wisdomimade@paucasesnovescifp.cat>
2025-10-30 10:43:35 +0100

    Refactor AvatarPage layout to improve structure and readability

--- lib/screens/avatar_page.dart
+++ lib/screens/avatar_page.dart
@@ -7,7 +7,25 @@ class AvatarPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Avatar Page')),
+      appBar: AppBar(
+        title: Text('Avatar Page'),
+        actions: [
+          Container(
+            margin:  EdgeInsets.only(right: 10),
+            child: Container(
+              padding: EdgeInsets.all(5),
+              child: CircleAvatar(
+                backgroundImage: NetworkImage('https://static.wikia.nocookie.net/minion/images/c/ca/Bob-from-the-minions-movie.jpg/revision/latest/scale-to-width-down/600?cb=20160316002021&path-prefix=es'),
+                radius: 30,
+              ),
+            ),
+          )
+        ],),
+        body: Center(
+          child: FadeInImage(
+            image: NetworkImage('https://static.wikia.nocookie.net/minion/images/c/ca/Bob-from-the-minions-movie.jpg/revision/latest/scale-to-width-down/600?cb=20160316002021&path-prefix=es'),
+            placeholder: AssetImage('assets/jar-loading.gif')),
+        ),
     );
   }
 }
